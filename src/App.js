9dc2cddc7428e5b11c8e6641d11a70501d1c2f8b
kumkumsingh <kumkumsingh.r@gmail.com>
2022-10-11 11:13:17 +0200

    redux toolkit async call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getPosts } from "./redux/features/postSlice"
 
 function App() {
+  const dispatch = useDispatch();
+  const { posts, loading } = useSelector((state) => state.post && state.post)
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [])
+  if(loading){
+    return <h2>...loading</h2>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {posts && posts.length && posts.map((item) => {
+        return <div key={item.id}>{item.title}</div>
+      })}
     </div>
   );
 }
